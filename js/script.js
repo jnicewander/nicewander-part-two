0df28dd75690bf8a7b598af7f5ff1be9c6edf4d6
J.Nicewander <jnicewander@live.com>
2020-02-26 11:14:33 -0500

    Created func to check user input and return destination.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 const vacationType = prompt('What kind of trip would you like to go on, musical, tropical, or adventurous?').toLowerCase();
 const groupSize = prompt('How many are in your group?');
 
-const destination = '';
+// Compare user input to determine destination
+function dest() {
+    let recommendation = '';
+
+    if (vacationType == 'musical') {
+        recommendation = 'New Orleans';
+    } else if (vacationType == 'tropical') {
+        recommendation = 'Beach Vacation in Mexico';
+    } else if (vacationType == 'adventurous') {
+        recommendation == 'Whitewater Rafting the Grand Canyon';
+    } else {
+        alert('Please choose either "musical", "tropical", or "adventurous".');
+        window.location.reload(false);
+    }
+    return recommendation;
+};
+
+
+const destination = dest();
 const transportMethod = '';
 const result = `You have a group of ${groupSize}, and would like to go on a ${vacationType} vacation. We recommend a trip to ${destination}, and with a group that size, you should take a ${transportMethod} to get there.`;
+console.log(result);
